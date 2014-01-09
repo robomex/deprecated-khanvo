d9a431c80ed42811c85562d5084ae992d19a31ac
robomex <atmorales@gmail.com>
2014-01-09 01:06:54 -0600

    need additional collections, separate side-nav links from current khan, figure out named routing/khanvo properties

---
+++ server/fixtures.js
@@ -0,0 +1,13 @@
+if (Posts.find().count() === 0) {
+	Posts.insert({
+		content: 'i am robomex'
+	});
+		
+	Posts.insert({
+		content: 'and this is khanvo'
+	});
+	
+	Posts.insert({
+		content: 'stfu'
+	});
+}
