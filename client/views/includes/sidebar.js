50e31fa87f3f95996aa585fcea7f64c6974872c7
Tony Morales <atmorales@gmail.com>
2014-01-24 19:32:29 -0600

    need to sort out the routing so that i can use the khanvo names - i need to do so before i can work on the sidebar navigation
    i.e. need actual use case routing since the activeRouteClass helper is
    looking for the name within the URL (and that is the functionality that
    i want with the actual product). need to sort out the _id in some place
    that i haven't quite found yet

--- client/views/includes/sidebar.js
+++ client/views/includes/sidebar.js
@@ -1,5 +1,14 @@
 Template.sidebar.helpers({
 	khanvo: function() {
 		return Khanvos.find();
+	},
+	activeRouteClass: function(/* route names */) {
+		var args = Array.prototype.slice.call(arguments, 0);
+		args.pop();
+
+		var active = _.any(args, function(name) {
+			return Router.current().route.name === name
+		});
+		return active && 'active';
 	}
 });
